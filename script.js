b11c19f40b18f07102af6b83db43e70c18977dd9
Renata L. R. de Sena <renatalrdesena@gmail.com>
2021-07-14 13:13:31 -0300

    Atualizando cores

--- script.js
+++ script.js
@@ -15,18 +15,18 @@ let food = {
 }
 
 function criarBG(){
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "lightyellow";
     context.fillRect(0,0,16*box,16*box);
 }
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
-        context.fillStyle = "green";
+        context.fillStyle = "pink";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
 function drawFood(){
-    context.fillStyle = "red";
+    context.fillStyle = "yellow";
     context.fillRect(food.x, food.y, box, box);
 }
 
